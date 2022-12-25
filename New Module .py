3f44833dc5ab36c97565682dfd77741fd13aa1d0
AGS12384836396 <91117189+AGS12384836396@users.noreply.github.com>
2022-12-25 18:58:26 +0530

    Add files via upload

---
+++ New Module .py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def Outmessage():
+    print('welcome to new module created by msg :')
+
+
+# In[2]:
+
+
+def welcome_by_Name(name):
+    print("Hi there {}. Good to meet you".format(name))
+
+
+# In[3]:
+
+
+def multiply():
+    user_input = int (input("please  provide your number. we will multiply it by 100 :"))
+    final_num = user_input*100
+    print(' your final number is {} '.format(final_num))
+
+
+# In[ ]:
+
+
+
+
